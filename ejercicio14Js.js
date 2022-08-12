98cfb5b8c8965117ccfc342bba40d1ba9d223464
AlexYukii <yukipaula16@gmail.com>
2022-08-12 13:42:55 +0200

    12/08/22 vainas hoy

--- ejercicio14Js.js
+++ ejercicio14Js.js
@@ -1,7 +1,19 @@
-const frutas = ["Hb","Bu","Pz","Ud","Bn","Bz","Gll","Tn"];
-if (frutas[i]=="Pz") {
-    frutas[i]
-}
+/*let frutas = ["Hb","Bu","Pz","Ud","Bn","Bz","Gll","Tn"];
+var feo = false;
+frutas.forEach(i =>  {
+   if (i == "Pz") {
+      feo = true;
+   }
+    if(feo) {
+      i = "Cz"
+    }
+});
+console.log(frutas); */
+let feo = ["Hb","Bu","Pz","Ud","Bn","Bz","Gll","Tn"];
+feo.fill("Cz",3)
+console.log(feo)
+
+
 
 
 
